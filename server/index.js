3b670ddec4821e9c3bac79c3d97147aff3ff0ddf
anusaraweerasooriya <109959317+anusaraweerasooriya@users.noreply.github.com>
2023-03-09 14:24:20 +0530

    setting up the folder structure and initializing the project

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+/* MIDDLEWARE CONFIGURATION =====================*/
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES =====================*/
+
+/* MONGOOSE SETUP =====================*/
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
