7c5260e9fc6b41b62c7090317d6825736a6c0265
Anusara Weerasooriya <109959317+anusaraweerasooriya@users.noreply.github.com>
2023-03-12 01:09:52 +0530

    Add files via upload

---
+++ client/src/user/pages/layout/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import Navbar from "user/components/Navbar";
+import Footer from "user/components/Footer";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ClientLayout = () => {
+  return (
+    <Box>
+      {/* NAVBAR */}
+      <Navbar />
+
+      {/* PAGE BODY */}
+      <Outlet />
+
+      {/* FOOTER */}
+      <Box>
+        <Footer />
+      </Box>
+    </Box>
+  );
+};
+
+export default ClientLayout;
