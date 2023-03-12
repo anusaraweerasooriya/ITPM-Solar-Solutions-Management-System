76895ab77658e14a4be35a0ed7420d8cff66d251
Dilshi Wanniarachchi <110255212+DilshiWanniarachchi@users.noreply.github.com>
2023-03-12 13:11:22 +0530

    created mongoose Rural Project Schema

---
+++ server/models/RuralProject.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const RuralProjectSchema = new mongoose.Schema(
+    {
+        projectName: {
+            type: String,
+            required: true,
+            min: 5,
+            max: 100,
+        },
+        location: {
+            type: String,
+            required: true,
+            max: 50,
+        },
+        projectType: {
+            type: String,
+            enum: ['Domestic', 'Commercial'],
+            required: true,
+        },
+        monthlyConsumption: {
+            type: Number,
+            required: true,
+        },
+        gridType: {
+            type: String,
+            enum: ['Off-grid', 'On-grid'],
+            required: true,
+        },
+        estimInitiateDate: {
+            type: Date,
+            required: true,
+        },
+        estimEndDate: {
+            type: Date,
+            required: true,
+        },
+        estimTotalCost: {
+            type: Number,
+            required: true,
+        },
+        imagePath: {
+            type: String,
+            default: "",
+        },
+        description: {
+            type: String,
+        },
+        currentAllocation: {
+            type: Number,
+            default: 0,
+        },
+        status: {
+            type: String,
+            default: "Pending",
+        },
+        
+    }
+);
+
+const RuralProject = mongoose.model("RuralProject", RuralProjectSchema);
+export default RuralProject;
