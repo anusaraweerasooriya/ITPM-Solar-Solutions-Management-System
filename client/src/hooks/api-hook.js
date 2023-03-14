910724d3cee0142999b414c9fb72b494eb2cddfa
anusaraweerasooriya <109959317+anusaraweerasooriya@users.noreply.github.com>
2023-03-14 16:22:04 +0530

    fixed some issues

--- client/src/hooks/api-hook.js
+++ client/src/hooks/api-hook.js
@@ -1,15 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const api = createApi({
-        baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_BASE_URL }),
-        reducerPath: "adminApi",
-        tagTypes: ["RuralProjects"],
-        endpoints: (build) => ({
-            getRuralProjects: build.query({
-                query: () => "projects/ruralProjects", 
-                providesTags: ["RuralProjects"],
-            }),
-        }),
-    });
+export const customApi = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_BASE_URL }),
+  reducerPath: "adminApi",
+  tagTypes: ["RuralProjects"],
 
-export const { useGetRuralProjectsQuery } = api;
+  endpoints: (build) => ({
+    getRuralProjects: build.query({
+      query: () => "projects/ruralProjects",
+      providesTags: ["RuralProjects"],
+    }),
+  }),
+});
+
+export const { useGetRuralProjectsQuery } = customApi;
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { configureStore, getDefaultMiddleware, combineReducers } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "hooks/auth-hook";
 import { Provider } from "react-redux";
 import {
@@ -17,21 +17,22 @@ import {
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
-import { api } from "hooks/api-hook";
+import { customApi } from "hooks/api-hook";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 const persistConfig = { key: "root", storage, version: 1 };
-const reducer = combineReducers({ [api.reducerPath]: api.reducer, authReducer });
-const persistReducers = persistReducer(persistConfig, reducer);
+
+const persistReducers = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: persistReducers,
+  [customApi.reducerPath]: customApi.reducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-    }).concat(api.middleware),
+    }).concat(customApi.middleware),
 });
 setupListeners(store.dispatch);
 
