32902eca1f607a71fc867b8a7cd44229173fbca9
Dilshi Wanniarachchi <110255212+DilshiWanniarachchi@users.noreply.github.com>
2023-03-16 09:09:15 +0530

    access RTK Query from the frontend

--- client/src/user/pages/donate/index.js
+++ client/src/user/pages/donate/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import {
   Box,
   Card,
+  CardActionArea,
   CardActions,
   CardContent,
+  CardMedia,
   Collapse,
   Button,
   Typography,
@@ -18,14 +20,33 @@ import DonateForm from "./donateForm";
 
 const Donate = () => {
   const { data } = useGetRuralProjectsQuery();
-
   console.log(data);
 
+  const isNonMobile = useMediaQuery("(min-width: 1000px");
+
   return (
-    <Box>
-      <SlideShow />
-      <DonateForm />
+    <>
+    <SlideShow />
+    <Box m="1.5rem 2.5rem">
+      {data ? (
+        <Box 
+          mt="20px"
+          display="grid"
+          gridTemplateColumns="repeat(3, minmax(0, 1fr))"
+          justifyContent="space-between"
+          rowGap="20px"
+          columnGap="1.33%"
+          sx={{
+            "& > div": { gridColumn: isNonMobile ? undefined : "span 3" }
+          }}
+        >
+          <div></div>
+        </Box>
+      ) : (
+        <></>
+      )}
     </Box>
+    </>
   );
 };
 
