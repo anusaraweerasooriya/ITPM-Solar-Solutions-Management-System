b6a1462c1c81c1fea36815139a86a5dfef9c2aa5
Janudi62 <janudikularathna@gmail.com>
2023-05-16 01:03:15 +0530

    Created Product Request Schema

---
+++ server/models/ProductRequest.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const ProductRequestSchema = new mongoose.Schema(
+    {
+        fullName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: String,
+            required: true,
+        },
+        date: {
+            type: Date,
+        },
+        Messages: String,
+    }
+);
+
+const ProductRequest = mongoose.model("productRequest", ProductRequestSchema);
+export default ProductRequest;
