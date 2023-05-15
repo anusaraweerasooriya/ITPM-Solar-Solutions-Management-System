91d7e4d55f7ddd2a5488e2a1c666144b0ec3e2fd
Janudi62 <janudikularathna@gmail.com>
2023-05-15 18:45:28 +0530

    Created Product View function

---
+++ client/src/admin/pages/product/productView.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useGetProductByIdQuery } from 'hooks/api-hook';
+import { Grid } from '@mui/material';
+
+const ProductView = ({prodId}) => {
+    const {data} = useGetProductByIdQuery({prodId});
+  return (
+    data && (
+        <Grid container>
+            <Grid xs={12} textAlign="center">
+                <Typography fontWeight="bold" fontSize="1rem" color="primary">{data.productName}</Typography>
+            </Grid>
+            <Grid xs={12} textAlign="center">
+                <Typography color="primary">{data.price}</Typography>
+                <hr></hr>
+            </Grid>
+
+            <Grid xs={4} mt="1rem">
+                <Typography fontWeight="bold">Product Type : </Typography>
+            </Grid>
+            <Grid xs={8} mt="1rem">
+                <Typography>{data.productType}</Typography>
+            </Grid>
+
+            <Grid xs={4} mt="1rem">
+                <Typography fontWeight="bold">Desctiption : </Typography>
+            </Grid>
+            <Grid xs={8} mt="1rem">
+                <Typography>{data.description}</Typography>
+            </Grid>
+
+            <Grid xs={4} mt="1rem">
+                <Typography fontWeight="bold">Category : </Typography>
+            </Grid>
+            <Grid xs={8} mt="1rem">
+                <Typography>{data.category}</Typography>
+            </Grid>
+        </Grid>
+    )
+  )
+}
+
+export default ProductView;
