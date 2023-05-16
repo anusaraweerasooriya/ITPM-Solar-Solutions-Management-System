8b933fdb15241fa9f5f89d51c0bc8695448ed08e
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-05-16 17:20:25 +0530

    Connected the api endpoint to frontend UI components and displayed all ProjectPlans

--- client/src/user/pages/profile/ProjectPlans/index.js
+++ client/src/user/pages/profile/ProjectPlans/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box, Grid, useMediaQuery } from "@mui/material";
 import Sidebar from "../components/Sidebar";
+import ProjectPlanCard from "./ProjectPlanCard";
 
 const UserProjectPlans = () => {
   const isNonMobileScreen = useMediaQuery("(min-width: 900px");
@@ -11,7 +12,10 @@ const UserProjectPlans = () => {
           {isNonMobileScreen && <Sidebar />}
         </Grid>
         <Grid item xs={8}>
-          <Box> ProjectPlans</Box>
+          <Box>
+            {" "}
+            <ProjectPlanCard />
+          </Box>
         </Grid>
       </Grid>
     </Box>
